68a0a9de3a7b00a09f48b076ebf2d8ae9398d5b7
tcely <tcely@users.noreply.github.com>
2026-03-30 06:07:31 -0400

    fix: read `GITHUB_EVENT_PATH` using @actions/github

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,42 @@
 // src/main.ts
 
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import Config from './config'
 import NoResponse from './no-response'
 
 async function run(): Promise<void> {
   try {
-    const eventName = process.env['GITHUB_EVENT_NAME']
-    core.debug(`Running action for event: ${eventName}`)
+    const eventName = github.context.eventName
+    const eventType =
+      'string' === typeof github.context.payload.action ? github.context.payload.action : undefined
+    core.info(`Running action for: name=${eventName} type=${eventType ?? ''}`)
 
     const config = new Config()
     const noResponse = new NoResponse(config)
 
-    if (eventName === 'schedule' || eventName === 'workflow_dispatch') {
+    if ('schedule' === eventName || 'workflow_dispatch' === eventName) {
       await noResponse.sweep()
-    } else if (eventName === 'issue_comment') {
+    } else if ('issue_comment' === eventName) {
       // React to comments on issues
       // When it was the author, we must update the issue
-      await noResponse.handleAuthorCommented()
-    } else if (eventName === 'issues') {
+      if ('created' === eventType) {
+        await noResponse.handleAuthorCommented()
+      } else {
+        core.info(`Unrecognized issue_comment type: ${eventType}`)
+      }
+    } else if ('issues' === eventName) {
       // React to issue events
       // Specifically removing the optional label,
       // after the issue was closed and removing the
       // author from the assigned users
-      const action = process.env['GITHUB_EVENT_ACTION']
 
-      if ('labeled' === action) {
+      if ('labeled' === eventType) {
         await noResponse.handleLabeled()
-      } else if ('closed' === action) {
+      } else if ('closed' === eventType) {
         await noResponse.handleClosedIssue()
+      } else {
+        core.info(`Unrecognized issues type: ${eventType}`)
       }
     } else {
       core.info(`This action was skipped. Unrecognized event: ${eventName}`)
